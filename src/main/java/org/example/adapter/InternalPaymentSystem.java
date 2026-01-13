162b987e3d32d207548120ae093035e2e9e2b19b
Nirav Talaviya <nirav@Niravs-MacBook-Pro.local>
2026-01-12 16:49:28 -0800

    Add logline

--- src/main/java/org/example/adapter/InternalPaymentSystem.java
+++ src/main/java/org/example/adapter/InternalPaymentSystem.java
@@ -2,6 +2,7 @@ package org.example.adapter;
 
 public class InternalPaymentSystem {
     public boolean processPayment(PaymentProcessor paymentProcessor) {
+        System.out.println("Internal Payment system processing payment");
         return paymentProcessor.processPayment();
     }
 }
